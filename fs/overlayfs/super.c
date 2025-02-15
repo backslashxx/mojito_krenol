afb3668ccdf49423bcf75de6dadbf320ae4e8cc6
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-16 02:14:24 +0800

    ovl: omit mount info if fake

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -310,6 +310,11 @@ static int ovl_show_options(struct seq_file *m, struct dentry *dentry)
 	struct super_block *sb = dentry->d_sb;
 	struct ovl_fs *ufs = sb->s_fs_info;
 
+	// 
+	if (strcmp(sb->s_type->name, "erofs") == 0) {
+		goto skip;
+	}
+
 	seq_show_option(m, "lowerdir", ufs->config.lowerdir);
 	if (ufs->config.upperdir) {
 		seq_show_option(m, "upperdir", ufs->config.upperdir);
@@ -326,6 +331,7 @@ static int ovl_show_options(struct seq_file *m, struct dentry *dentry)
 	if (ufs->config.override_creds != ovl_override_creds_def)
 		seq_show_option(m, "override_creds",
 				ufs->config.override_creds ? "on" : "off");
+skip:
 	return 0;
 }
 
